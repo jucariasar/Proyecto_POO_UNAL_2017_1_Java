bca8607f2e1cb339d93bf7ff9f942237add2021c
jucariasar <jucariasar@unal.edu.co>
2017-05-07 16:46:35 -0500

    Inicio de modificación Clase Empleado

--- src/java/modelos/Empleado.java
+++ src/java/modelos/Empleado.java
@@ -1,29 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package modelos;
 
 import java.util.*;
 
-/**
- *
- * @author Pablo
- */
 public class Empleado {
-    
-    public static List<Empleado> empleados = new ArrayList<Empleado>() ;
-        //{
-        //    add(new Empleado(1,"hjk","dfghjkm","rtyui","sdfgy"));
-           
+
+    public static List<Empleado> empleados = new ArrayList<Empleado>();
 
     private int ident = 0;
     private String nombre = "";
-    private String Apellido = "";
+    private String apellido = "";
     private int numElementPres = 0;
     private String roll = "";
     private String email = "";
+
+    public Empleado(int ident, String nombre, String apellido, String roll, String email) {
+        setIdent(ident);
+        setNombre(nombre);
+        setApellido(apellido);
+        setRoll(roll);
+        setEmail(email);
+
+    }
 
     public int getIdent() {
         return ident;
@@ -42,11 +39,11 @@ public class Empleado {
     }
 
     public String getApellido() {
-        return Apellido;
+        return apellido;
     }
 
-    public void setApellido(String Apellido) {
-        this.Apellido = Apellido;
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
     }
 
     public int getNumElementPres() {
@@ -75,19 +72,7 @@ public class Empleado {
 
     @Override
     public String toString() {
-        return "Empleado{" + "ident=" + ident + ", nombre=" + nombre + ", Apellido=" + Apellido + ", numElementPres=" + numElementPres + ", roll=" + roll + ", email=" + email + '}';
+        return "Empleado{" + "ident=" + ident + ", nombre=" + nombre + ", Apellido=" + apellido + ", numElementPres=" + numElementPres + ", roll=" + roll + ", email=" + email + '}';
     }
-
-    public Empleado(int ident,String nombre,String Apellido,String roll,String email) {
-        setIdent(ident);
-        setNombre(nombre);
-        setApellido(Apellido);
-        setRoll(roll);
-        setEmail(email);       
-        
-    }
-
-    
-    
 
 }
